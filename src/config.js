4a1c4af90bd1a9e2ed10542bb547e345973ed54f
Viktor Kolind <s204166@student.dtu.dk>
2025-02-03 14:25:48 +0100

    working on the keyboard
    added numbers, alphabet selection, caps lock functionality, skeleton for navigation and editing

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+import { act } from "react";
+
 export const config = {
   layouts: {
     "writing": {
@@ -6,6 +8,16 @@ export const config = {
         {
           type: "textarea",
           colspan: 2 // This tile spans 2 columns
+        },
+        {
+          type: "delete",
+          label: "DELETE",
+          action: { type: "delete_letter" }
+        },
+        {
+          type: "switch",
+          label: "ABC",
+          action: { type: "switch_layout", layout: "writing_submenu" }
         },
         {
           type: "letter",
@@ -53,11 +65,6 @@ export const config = {
           type: "letter",
           label: "H",
           action: { type: "enter_letter", value: "H" }
-        },
-        {
-          type: "switch",
-          label: "ABC",
-          action: { type: "switch_layout", layout: "writing_submenu" }
         }
       ]
     },
@@ -68,15 +75,55 @@ export const config = {
           colspan: 2 // This tile spans 2 columns
         },
         {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
           type: "switch",
           label: "back",
           action: { type: "switch_layout", layout: "main_menu" }
         },
         {
-          type: "letter",
-          label: "J",
-          action: { type: "enter_letter", value: "J" }
+          type: "switch",
+          label: "ABCDEFGH",
+          action: { type: "switch_layout", layout: "ABCDEFGH_menu" }
         },
+        {
+          type: "switch",
+          label: "IJKLMNOP",
+          action: { type: "switch_layout", layout: "IJKLMNOP_menu" }
+        },
+        {
+          type: "switch",
+          label: "QRSTUVWX",
+          action: { type: "switch_layout", layout: "QRSTUVWX_menu" }
+        },
+        {
+          type: "switch",
+          label: "YZÆØÅ,?.._",
+          action: { type: "switch_layout", layout: "YZÆØÅ,?.._menu" }
+        },
+        {
+          type: "switch",
+          label: "0-9",
+          action: { type: "switch_layout", layout: "numbers" }
+        },
+        {
+          type: "toggle_case",
+          label: "CAPS",
+          action: { type: "toggle_case" }
+        },
+        {
+          type: "switch",
+          label: "Navigation",
+          action: { type: "switch_layout", layout: "navigation_menu" }
+        },
+        {
+          type : "switch",
+          label : "Edit",
+          action : { type : "switch_layout", layout : "edit_menu"}
+        }
 
       ]
 
@@ -86,6 +133,16 @@ export const config = {
         {
           type: "textarea",
           colspan: 2
+        },
+        {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
+          type : "switch",
+          label : "back",
+          action : { type: "switch_layout", layout : "writing"}
         },
         {
           type: "letter",
@@ -114,28 +171,16 @@ export const config = {
         },
         {
           type: "switch",
-          label: "ABC",
-          action: { type: "switch_layout", layout: "default" }
+          label: "67890",
+          action: { type: "switch_layout", layout: "67890_menu" }
         },
         {
-          type: "letter",
-          label: "6",
-          action: { type: "enter_letter", value: "6" }
+          type : "empty",
+          label : ""
         },
         {
-          type: "letter",
-          label: "7",
-          action: { type: "enter_letter", value: "7" }
-        },
-        {
-          type: "letter",
-          label: "8",
-          action: { type: "enter_letter", value: "8" }
-        },
-        {
-          type: "switch",
-          label: "ABC",
-          action: { type: "switch_layout", layout: "default" }
+          type : "empty",
+          label : ""
         }
       ]
     },
@@ -190,6 +235,572 @@ export const config = {
           label: "",
         },
       ]
+    },
+    "ABCDEFGH_menu": {
+      tiles: [
+        {
+          type: "textarea",
+          colspan: 2 // This tile spans 2 columns
+        },
+        {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
+          type: "switch",
+          label: "back",
+          action: { type: "switch_layout", layout: "writing" }
+        },
+        {
+          type: "letter",
+          label: "A",
+          action: { type: "enter_letter", value: "A" }
+        },
+        {
+          type: "letter",
+          label: "B",
+          action: { type: "enter_letter", value: "B" }
+        },
+        {
+          type: "letter",
+          label: "C",
+          action: { type: "enter_letter", value: "C" }
+        },
+        {
+          type: "letter",
+          label: "D",
+          action: { type: "enter_letter", value: "D" }
+        },
+        {
+          type: "letter",
+          label: "E",
+          action: { type: "enter_letter", value: "E" }
+        },
+        {
+          type: "letter",
+          label: "F",
+          action: { type: "enter_letter", value: "F" }
+        },
+        {
+          type: "letter",
+          label: "G",
+          action: { type: "enter_letter", value: "G" }
+        },
+        {
+          type: "letter",
+          label: "H",
+          action: { type: "enter_letter", value: "H" }
+        }
+      ]
+    },
+    "IJKLMNOP_menu": {
+      tiles: [
+        {
+          type: "textarea",
+          colspan: 2 // This tile spans 2 columns
+        },
+        {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
+          type: "switch",
+          label: "back",
+          action: { type: "switch_layout", layout: "writing" }
+        },
+        {
+          type: "letter",
+          label: "I",
+          action: { type: "enter_letter", value: "I" }
+        },
+        {
+          type: "letter",
+          label: "J",
+          action: { type: "enter_letter", value: "J" }
+        },
+        {
+          type: "letter",
+          label: "K",
+          action: { type: "enter_letter", value: "K" }
+        },
+        {
+          type: "letter",
+          label: "L",
+          action: { type: "enter_letter", value: "L" }
+        },
+        {
+          type: "letter",
+          label: "M",
+          action: { type: "enter_letter", value: "M" }
+        },
+        {
+          type: "letter",
+          label: "N",
+          action: { type: "enter_letter", value: "N" }
+        },
+        {
+          type: "letter",
+          label: "O",
+          action: { type: "enter_letter", value: "O" }
+        },
+        {
+          type: "letter",
+          label: "P",
+          action: { type: "enter_letter", value: "P" }
+        }
+      ]
+    },
+    "QRSTUVWX_menu": {
+      tiles: [
+        {
+          type: "textarea",
+          colspan: 2 // This tile spans 2 columns
+        },
+        {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
+          type: "switch",
+          label: "back",
+          action: { type: "switch_layout", layout: "writing" }
+        },
+        {
+          type: "letter",
+          label: "Q",
+          action: { type: "enter_letter", value: "Q" }
+        },
+        {
+          type: "letter",
+          label: "R",
+          action: { type: "enter_letter", value: "R" }
+        },
+        {
+          type: "letter",
+          label: "S",
+          action: { type: "enter_letter", value: "S" }
+        },
+        {
+          type: "letter",
+          label: "T",
+          action: { type: "enter_letter", value: "T" }
+        },
+        {
+          type: "letter",
+          label: "U",
+          action: { type: "enter_letter", value: "U" }
+        },
+        {
+          type: "letter",
+          label: "V",
+          action: { type: "enter_letter", value: "V" }
+        },
+        {
+          type: "letter",
+          label: "W",
+          action: { type: "enter_letter", value: "W" }
+        },
+        {
+          type: "letter",
+          label: "X",
+          action: { type: "enter_letter", value: "X" }
+        }
+      ]
+    },
+    "YZÆØÅ,?.._menu": {
+      tiles: [
+        {
+          type: "textarea",
+          colspan: 2 // This tile spans 2 columns
+        },
+        {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
+          type: "switch",
+          label: "back",
+          action: { type: "switch_layout", layout: "writing" }
+        },
+        {
+          type: "letter",
+          label: "Y",
+          action: { type: "enter_letter", value: "Y" }
+        },
+        {
+          type: "letter",
+          label: "Z",
+          action: { type: "enter_letter", value: "Z" }
+        },
+        {
+          type: "letter",
+          label: "Æ",
+          action: { type: "enter_letter", value: "Æ" }
+        },
+        {
+          type: "letter",
+          label: "Ø",
+          action: { type: "enter_letter", value: "Ø" }
+        },
+        {
+          type: "letter",
+          label: "Å",
+          action: { type: "enter_letter", value: "Å" }
+        },
+        {
+          type: "letter",
+          label: ",",
+          action: { type: "enter_letter", value: "," }
+        },
+        {
+          type: "switch",
+          label: "():?+./=",
+          action: { type: "switch_layout", layout: "():?+./=_menu" }
+        },
+        {
+          type: "switch",
+          label: "More",
+          action: { type: "switch_layout", layout: "\'\"%@;!*" }
+        },
+        
+      ]
+    },
+    "():?+./=_menu": {
+        tiles: [
+          {
+            type: "textarea",
+            colspan: 2 // This tile spans 2 columns
+          },
+          {
+            type: "letter",
+            label: ".",
+            action: { type: "enter_letter", value: "." }
+          },
+          {
+            type: "switch",
+            label: "back",
+            action: { type: "switch_layout", layout: "YZÆØÅ,?.._menu" }
+          },
+          {
+            type: "letter",
+            label: "(",
+            action: { type: "enter_letter", value: "(" }
+          },
+          {
+            type: "letter",
+            label: ")",
+            action: { type: "enter_letter", value: ")" }
+          },
+          {
+            type: "letter",
+            label: ":",
+            action: { type: "enter_letter", value: ":" }
+          },
+          {
+            type: "letter",
+            label: "?",
+            action: { type: "enter_letter", value: "?" }
+          },
+          {
+            type: "letter",
+            label: "+",
+            action: { type: "enter_letter", value: "+" }
+          },
+          {
+            type: "letter",
+            label: "/",
+            action: { type: "enter_letter", value: "/" }
+          },
+          {
+            type: "letter",
+            label: "=",
+            action: { type: "enter_letter", value: "=" }
+          },
+          {
+            type: "letter",
+            label: "_",
+            action: { type: "enter_letter", value: "_" }
+          }
+        ]
+    },
+    "\'\"%@;!*": {
+        tiles: [
+          {
+            type: "textarea",
+            colspan: 2 // This tile spans 2 columns
+          },
+          {
+            type: "letter",
+            label: ".",
+            action: { type: "enter_letter", value: "." }
+          },
+          {
+            type: "switch",
+            label: "back",
+            action: { type: "switch_layout", layout: "YZÆØÅ,?.._menu" }
+          },
+          {
+            type: "letter",
+            label: "\'",
+            action: { type: "enter_letter", value: "\'" }
+          },
+          {
+            type: "letter",
+            label: "\"",
+            action: { type: "enter_letter", value: "\"" }
+          },
+          {
+            type: "letter",
+            label: "%",
+            action: { type: "enter_letter", value: "%" }
+          },
+          {
+            type: "letter",
+            label: "@",
+            action: { type: "enter_letter", value: "@" }
+          },
+          {
+            type: "letter",
+            label: ";",
+            action: { type: "enter_letter", value: ";" }
+          },
+          {
+            type: "letter",
+            label: "!",
+            action: { type: "enter_letter", value: "!" }
+          },
+          {
+            type: "letter",
+            label: "*",
+            action: { type: "enter_letter", value: "*" }
+          },
+          {
+            type: "switch",
+            label: "More9870",
+            action: { type: "switch_layout", layout: "$£€#_\\_menu" }
+          }
+        ]
+    },
+    "$£€#_\\_menu":{
+        tiles: [
+          {
+            type: "textarea",
+            colspan: 2 // This tile spans 2 columns
+          },
+          {
+            type: "letter",
+            label: ".",
+            action: { type: "enter_letter", value: "." }
+          },
+          {
+            type: "switch",
+            label: "back",
+            action: { type: "switch_layout", layout: "\'\"%@;!*" }
+          },
+          {
+            type: "letter",
+            label: "$",
+            action: { type: "enter_letter", value: "$" }
+          },
+          {
+            type: "letter",
+            label: "£",
+            action: { type: "enter_letter", value: "£" }
+          },
+          {
+            type: "letter",
+            label: "€",
+            action: { type: "enter_letter", value: "€" }
+          },
+          {
+            type:"empty",
+            label: ""
+          },
+          {
+            type: "letter",
+            label: "#",
+            action: { type: "enter_letter", value: "#" }
+          },
+          {
+            type: "letter",
+            label: "\\",
+            action: { type: "enter_letter", value: "\\" }
+          },
+          {
+            type: "letter",
+            label: "_",
+            action: { type: "enter_letter", value: "_" }
+          },
+          {
+            type:"empty",
+            label: ""
+          },
+      ]
+    
+    },
+    "67890_menu": {
+      tiles: [
+        {
+          type: "textarea",
+          colspan: 2 // This tile spans 2 columns
+        },
+        {
+          type: "letter",
+          label: ".",
+          action: { type: "enter_letter", value: "." }
+        },
+        {
+          type: "switch",
+          label: "back",
+          action: { type: "switch_layout", layout: "writing" }
+        },
+        {
+          type: "letter",
+          label: "6",
+          action: { type: "enter_letter", value: "6" }
+        },
+        {
+          type: "letter",
+          label: "7",
+          action: { type: "enter_letter", value: "7" }
+        },
+        {
+          type: "letter",
+          label: "8",
+          action: { type: "enter_letter", value: "8" }
+        },
+        {
+          type: "letter",
+          label: "9",
+          action: { type: "enter_letter", value: "9" }
+        },
+        {
+          type: "letter",
+          label: "0",
+          action: { type: "enter_letter", value: "0" }
+        },
+        {
+          type: "empty",
+          label: ""
+        },
+        {
+          type: "empty",
+          label: ""
+        },
+        {
+          type: "empty",
+          label: ""
+        }
+      ]
+    },
+
+    "navigation_menu" : {
+      tiles : [
+        {
+          type: "textarea",
+          colspan: 2
+        },
+        {
+          label : "Edit",
+        },
+        {
+          type : "switch",
+          label : "Back",
+          action : { type: "switch_layout", layout : "writing"}
+        },
+        {
+          type : "cursor",
+          label : "cursor to the left",
+          action : { type: "cursor", direction : "left"}
+        },
+        {
+          type : "cursor",
+          label : "cursor to the right",
+          action : { type: "cursor", direction : "right"}
+        },
+        {
+          type : "cursor",
+          label : "cursor up",
+          action : { type: "cursor", direction : "up"}
+        },
+        {
+          label : "More functions",
+        },
+        {
+          type: "newline",
+          label : "newline",
+          action : { type: "newline", value: "\n"}
+        },
+        {
+          type : "empty",
+          label : "",
+        },
+        {
+          type : "cursor",
+          label : "cursor down",
+          action : { type: "cursor", direction : "down"}
+        },
+        {
+          label : "File",
+        }
+        
+      ]
+    },
+    "edit_menu" : {
+      tiles : [
+        {
+          type: "textarea",
+          colspan: 2
+        },
+        {
+          type: "switch",
+          label: "Navigation",
+          action: { type: "switch_layout", layout: "navigation_menu" }
+        },
+        {
+          type: "switch",
+          label: "Back",
+          action: { type: "switch_layout", layout: "writing" }
+
+        },
+        {
+          type : "asdf",
+          label : "Delete Section"
+        },
+        {
+          type : "asdf",
+          label : "Delete Line"
+        },
+        {
+          type : "asdf",
+          label : "Delete Word"
+        },
+        {
+          type : "asdf",
+          label : "Delete Letter"
+        },
+        {
+          type : "empty",
+          label : ""
+        },
+        {
+          type : "empty",
+          label : ""
+        },
+        {
+          type : "asdf",
+          label : "Undo"
+        },
+        {
+          type : "Switch",
+          label : "File",
+          action : { type: "switch_layout", layout: "file_menu"}
+        },
+      ]
     }
-  }
+}
 };
